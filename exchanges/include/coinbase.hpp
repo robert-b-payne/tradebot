95780135a615b326d63afec95cf9400a740377fe
Robert Payne <rp1242@gmail.com>
2024-01-07 09:13:22 -0600

    add coinbase

---
+++ exchanges/include/coinbase.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "exchange.hpp"
+#include <iostream>
+#include "common.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+class Binance : public Exchange 
+{
+public:
+    std::string getBalance() override;
+    void fetchPriceList();
+
+    float getPriceETHBTC();
+    std::string getExchangeName();
+private:
+    std::string baseURL_ = "https://api-public.sandbox.exchange.coinbase.com";
+    std::string priceURL_ = "https://www.binance.com/api/v3/ticker/price";
+    std::string priceList_;
+
+    float parseETHBTC();
+
+    float priceETHBTC_;
+
+    std::string exchangeName_ = "Coinbase";
+};
